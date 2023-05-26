7df13b960496d04efd308104c6815a68636d6c5b
Bruno Ferrari <brunobferrari@gmail.com>
2023-05-26 16:20:09 -0300

    Participante, Atividade e banco h2

---
+++ src/main/java/com/bruno/desafio2/Entities/Atividade.java
@@ -0,0 +1,75 @@
+package com.bruno.desafio2.Entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_atividade")
+public class Atividade {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	private String nome;
+	private String email;
+	private String descricao;
+	private Double preco;
+	
+	public Atividade() {		
+	}
+
+	public Atividade(Integer id, String nome, String email, String descricao, Double preco) {
+		this.id = id;
+		this.nome = nome;
+		this.email = email;
+		this.descricao = descricao;
+		this.preco = preco;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public Double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(Double preco) {
+		this.preco = preco;
+	}
+	
+	
+	
+
+}
